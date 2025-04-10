9e60102e473626209edcb71dfa6abe663843cf1a
Mohamad Billy <koncoweb@gmail.com>
2025-04-10 15:19:20 +0000

    tambahkan bottom nav seperti di home ke halaman /products

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,12 +2,15 @@
 
 import React from 'react';
 import ProductList from "@/components/ProductList";
+import TabBar from "@/components/TabBar";
 
 const ProductsPage: React.FC = () => {
   return (
-    <div className="container mx-auto py-6">
-      <h1 className="text-2xl font-semibold mb-4">Products Management</h1>
-      <ProductList/>
+    <div className="flex flex-col h-screen">
+      <div className="flex-1 overflow-y-auto">
+        <ProductList/>
+      </div>
+      <TabBar />
     </div>
   );
 };
