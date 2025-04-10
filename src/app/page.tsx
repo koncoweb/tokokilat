d00f26e24dde343d31fa006f8083560044c8beba
Mohamad Billy <koncoweb@gmail.com>
2025-04-10 15:21:42 +0000

    tambahkan header seperti aplikasi di home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,14 @@ import {useEffect} from "react";
 import {auth} from "@/lib/firebase";
 import {useRouter} from "next/navigation";
 import {onAuthStateChanged} from "firebase/auth";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
 
 export default function Home() {
   const router = useRouter();
@@ -22,6 +30,19 @@ export default function Home() {
 
   return (
     <div className="flex flex-col h-screen">
+           <header className="bg-secondary p-4">
+        <Card className="w-full">
+            <CardHeader>
+                <CardTitle>TokoKilat</CardTitle>
+                <CardDescription>Selamat datang di TokoKilat</CardDescription>
+            </CardHeader>
+            <CardContent>
+            Nikmati pengalaman belanja yang menyenangkan dan cepat!
+            </CardContent>
+            <CardFooter>
+              </CardFooter>
+        </Card>
+      </header>
       <main className="flex-1 overflow-y-auto">
         <HomeTab/>
       </main>
@@ -29,4 +50,5 @@ export default function Home() {
     </div>
   );
 }
+
 
